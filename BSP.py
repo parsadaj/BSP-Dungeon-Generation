d5d164714f492ddb51c0bc4219dea370ed47e9cb
Parsa <53321170+parsadaj@users.noreply.github.com>
2024-02-19 17:26:23 +0330

    BSPArray added, corridor for some nodes added

--- BSP.py
+++ BSP.py
@@ -10,6 +10,7 @@ class BSPNode:
         self.right = None
         self.parent = None
         self.room = None
+        self.corridor = None
         self.split = None
         
     def create_room(self):
@@ -19,50 +20,52 @@ class BSPNode:
         room_y = np.random.randint(self.y, self.y + (self.h - room_height))
         self.room = (room_x, room_y, room_width, room_height)
         
-    def connect_rooms(self):
-        pass
 
-
-def connect_two_rooms(room1, room2):
-    x1, y1, w1, h1 = room1
-    x2, y2, w2, h2 = room2
-    cx1 = np.random.randint(x1, x1 + w1)
-    cx2 = np.random.randint(x2, x2 + w2)
-    cy1 = np.random.randint(y1, y1 + h1)
-    cy2 = np.random.randint(y2, y2 + h2)
+class BSPArray:
+    def __init__(self, w, h):
+        self.array = np.zeros((h, w))
     
-    if np.random.rand() > 0.5:
-        corridors = [(cx1, cy1), (cx1, cy2), (cx2, cy2)]
-    else:
-        corridors = [(cx1, cy1), (cx2, cy1), (cx2, cy2)]
+    def __call__(self):
+        return self.array
     
-    for corridor in corridors:
-        if corridor[0] != cx1:
-            connect_horizontal(corridor[0], cx1, corridor[1])
-        if corridor[0] != cx2:
-            connect_horizontal(corridor[0], cx2, corridor[1])
-        if corridor[1] != cy1:
-            connect_vertical(corridor[1], cy1, corridor[0])
-        if corridor[1] != cy2:
-            connect_vertical(corridor[1], cy2, corridor[0])
+    def draw_room(self, room):
+        room_x, room_y, room_w, room_h = room
+        self.array[room_y: room_y+room_h, room_x: room_x+room_w] = 1
+    
+    def draw_straight_corridor(self, corridor, corridor_w):
+        start, end = corridor
+        x1, y1 = start
+        x2, y2 = end
+        
+        if x1 != x2 and y1 != y2:
+            raise
+        
+        if x1 == x2:
+            x = x1 - corridor_w//2
+            self.array[min([y1, y2]): max([y1, y2]), x: x+corridor_w] = 1
             
+        if y1 == y2:
+            y = y1 - corridor_w//2
+            self.array[y: y+corridor_w, min([x1,x2]):max([x1,x2])] = 1
+    
 class BSPGenerator:
-    def __init__(self, w, h, min_w=None, min_h=None, corridor_w=5):
+    def __init__(self, w, h, min_w=None, min_h=None, corridor_w=3):
         self.w = w
         self.h = h
         self.min_w = min_w if min_w is not None else w // 3
         self.min_h = min_h if min_h is not None else h // 3
-        self.array = np.zeros((h, w))
+        self.array = BSPArray(w, h)
         self.rooms = []
+        self.corridors = []
         self.nodes = []
-        self.corridor_w = 5
+        self.corridor_w = corridor_w
 
     def generate_dungeon(self):
         self.root = BSPNode(0, 0, self.w, self.h)
         self.bsp_tree_recursive(self.root)
-        # connect_rooms(root)
         self.generate_array()
-        return self.array
+        self.connect_rooms_with_same_parents()
+        return self.array()
 
     def bsp_tree_recursive(self, node: BSPNode):
         self.nodes.append(node)
@@ -83,10 +86,9 @@ class BSPGenerator:
         self.bsp_tree_recursive(node.right)
         
     def generate_array(self):
-        for (room_x, room_y, room_w, room_h) in self.rooms:
-            self.array[room_y: room_y+room_h, room_x: room_x+room_w] = 1
+        for room in self.rooms:
+            self.array.draw_room(room)
             
-
     def split_horizontally(self, node: BSPNode):
         split = np.random.randint(self.min_w//2, node.w - self.min_w//2)
         node.left = BSPNode(node.x,          node.y, split,              node.h)
@@ -98,5 +100,68 @@ class BSPGenerator:
         split = np.random.randint(self.min_h//2, node.h - self.min_h//2)
         node.left = BSPNode(node.x, node.y, node.w, split)
         node.right = BSPNode(node.x, node.y + split, node.w, node.h - split)
+    
+    def connect_rooms_with_same_parents(self):
+        for node in self.nodes:
+            if node.split == 'v':
+                if node.left.room is not None and node.right.room is not None:
+                    node.corridor = self.connect_rooms_v(node.left.room, node.right.room)
+            elif node.split == 'h':
+                if node.left.room is not None and node.right.room is not None:
+                    node.corridor = self.connect_rooms_h(node.left.room, node.right.room)
+
+    def connect_rooms_v(self, room1, room2):
+        x1, y1, w1, h1 = room1
+        x2, y2, w2, h2 = room2
+        
+        overlap = find_overlap(x1, x1+w1, x2, x2+w2)
+        if overlap is not None:
+            xo1, xo2 = overlap
+            xo2 = xo2 - self.corridor_w//2
+            xo1 = xo1 + self.corridor_w//2
+            
+            x_overlap_start = min([xo1, xo2])
+            x_overlap_end = max([xo1, xo2])
+            x = np.random.randint(x_overlap_start, x_overlap_end+1)
+            y_start = y1 + h1 if y2 > y1 else y2 + h2
+            y_end = y2 if y2 > y1 else y1
+            corridor = (x, y_start), (x, y_end)
+            self.corridors.append(corridor)
+            self.array.draw_straight_corridor(corridor, self.corridor_w)
+            return corridor
+            
+    def connect_rooms_h(self, room1, room2):
+        x1, y1, w1, h1 = room1
+        x2, y2, w2, h2 = room2
+        
+        overlap = find_overlap(y1, y1+h1, y2, y2+h2)
+        if overlap is not None:
+            yo1, yo2 = overlap
+            yo2 = yo2 - self.corridor_w//2
+            yo1 = yo1 + self.corridor_w//2
+            
+            y_overlap_start = min([yo1, yo2])
+            y_overlap_end = max([yo1, yo2])
+            y = np.random.randint(y_overlap_start, y_overlap_end+1)
+            x_start = x1 + w1 if x2 > x1 else x2 + w2
+            x_end = x2 if x2 > x1 else x1
+            corridor = (x_start, y), (x_end, y)
+            self.corridors.append(corridor)
+            self.array.draw_straight_corridor(corridor, self.corridor_w)
+            return corridor
+        
+def find_overlap(z1_start, z1_end, z2_start, z2_end):
+    # Check if there's no overlap
+    if z1_end < z2_start or z2_end < z1_start:
+        return None
+    
+    # If there's overlap, calculate overlap interval
+    x_overlap_start = max(z1_start, z2_start)
+    x_overlap_end = min(z1_end, z2_end)
+    
+    return x_overlap_start, x_overlap_end
+
+        
+                
 
 
